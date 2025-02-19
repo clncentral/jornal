108a330aa9b5b168bf3d58662bd75020d454581d
Elias <cln.central@gmail.com>
2025-02-19 10:42:44 -0300

    Add files via upload
    Signed-off-by: Elias <cln.central@gmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
-main.js
+// main.js
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+});
